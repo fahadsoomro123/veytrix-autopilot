d8459c5524a3227c0c7215662b9baba39d864b3c
Fahad Hussain <fahadsoomro123@gmail.com>
2026-09-19 17:58:19 +0500

    fix: import Typeface for voice surface

--- android/autopilot/src/main/java/com/veytrix/autopilot/VeytrixVoiceView.java
+++ android/autopilot/src/main/java/com/veytrix/autopilot/VeytrixVoiceView.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
